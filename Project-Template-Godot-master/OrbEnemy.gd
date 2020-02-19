638d8b9462693c264e31d3e373198e66f397d555
Hayasak-a <59937605+Hayasak-a@users.noreply.github.com>
2020-02-18 23:52:55 -0500

    First Start
    Began work on the project. Not finished

---
+++ Project-Template-Godot-master/OrbEnemy.gd
@@ -0,0 +1,17 @@
+extends KinematicBody2D
+
+export (int) var speed = 200
+
+var velocity = Vector2()
+
+func get_input():
+    velocity = Vector2()
+    velocity.y += 1
+    velocity = velocity.normalized() * speed
+    
+func _physics_process(delta):
+    get_input()
+    velocity = move_and_slide(velocity)
+    
+    
+    get_tree().change_scene("res://Scenes/End.tscn")
